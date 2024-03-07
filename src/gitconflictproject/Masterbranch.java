7f93fc1580c91f931d16fb16f88283ea5be3c4eb
rahuls75 <161546561+rahuls75@users.noreply.github.com>
2024-03-07 15:24:04 +0530

    final change on master branch

--- src/gitconflictproject/Masterbranch.java
+++ src/gitconflictproject/Masterbranch.java
@@ -5,7 +5,7 @@ public class Masterbranch {
 	
 	public static void main (String[]args) {
 	
-	System.out.println("This is master branch code");
+	System.out.println("This is master branch code is no longer avialble");
 	
 }
 }
