c4a108e6881a76f3f24fcbe721070f31f7447aed
rahuls75 <161546561+rahuls75@users.noreply.github.com>
2024-03-07 15:16:23 +0530

    my personal branch commit

--- src/gitconflictproject/Masterbranch.java
+++ src/gitconflictproject/Masterbranch.java
@@ -6,6 +6,6 @@ public class Masterbranch {
 	public static void main (String[]args) {
 	
 	System.out.println("This is master branch code");
-	
+	System.out.println("This code is from ny branch");
 }
 }
